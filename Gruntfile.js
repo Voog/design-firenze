e201b0fee25167d0fcf58f2c3b7d403c4a447b10
kaspar-naaber <kaspar.naaber@outlook.com>
2014-07-25 13:23:50 +0300

    Fix Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -109,10 +109,7 @@ module.exports = function(grunt) {
           '!*.min.css'
         ],
         dest: 'stylesheets/',
-        ext: '.min.css',
-        options: {
-          spawn: false
-        }
+        ext: '.min.css'
       }
     },
 
